3b3e6ba9907ca4b1b5bc1fa0de31ea494b70c406
abacaj@tmpg.com <abacaj1@gmail.com>
2014-09-10 11:12:53 -0400

    Removed Memcache

--- movies_script.php
+++ movies_script.php
@@ -3,14 +3,6 @@
 require_once 'medoo.min.php';
 require_once 'movieObject.php';
 
-// Connection constants
-define('MEMCACHED_HOST', '127.0.0.1');
-define('MEMCACHED_PORT', '11211');
- 
-// Connection creation
-$memcache = new Memcache;
-$cacheAvailable = $memcache->connect(MEMCACHED_HOST, MEMCACHED_PORT);
-
 // initialize database object
 $db = new medoo();
 
